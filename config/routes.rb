d5c1f2264c3dfa51308fd11fe8cc2d96d241466f
didierClin <didier.c62@numericable.fr>
2016-02-08 14:35:59 +0100

    Ajout page layout et relations

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
 
+  root to: "posts#index"
+  # root 'posts#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
